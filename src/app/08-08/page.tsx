47d4206ec176c7cc86f661c5abe18e123286addd
tialiancadivergente <ti@aliancadivergente.com.br>
2026-08-06 15:01:07 -0300

    page 08 08

---
+++ src/app/08-08/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main
+      className="flex flex-col items-center justify-center bg-areia-claro font-spectral"
+      role="main"
+      aria-label="Página inicial"
+    >
+      08-08
+    </main>
+  );
+}
